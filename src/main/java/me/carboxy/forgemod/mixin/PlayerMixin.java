67444934afccec5bbcac4e435e5de8fb7bb8d71e
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-10-05 01:18:36 +0530

    Add latest changes

--- src/main/java/me/carboxy/forgemod/mixin/PlayerMixin.java
+++ src/main/java/me/carboxy/forgemod/mixin/PlayerMixin.java
@@ -1,27 +1,56 @@
 package me.carboxy.forgemod.mixin;
 
 import org.spongepowered.asm.mixin.injection.Inject;
+
+import java.util.Map;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import me.carboxy.forgemod.CarboxyForgeMod;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin {
     @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("TAIL"), cancellable = true)
     protected void onDrop(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> cir) {
-        if (stack.getItem() instanceof BlockItem) {
-            Block block = ((BlockItem) stack.getItem()).getBlock();
-            if (block instanceof Block && block.getName().toString().contains("ore")) {
-                Player player = (Player) (Object) this;
-                player.displayClientMessage(Component.literal("You dropped an ore!"), false);
+        if (!(stack.getItem() instanceof Item)) {
+            return;
+        }
+
+        Map<Enchantment, Integer> itemEnchants = stack.getAllEnchantments();
+        CarboxyForgeMod.LOGGER.info("[PlayerMixin] onDrop");
+        
+        boolean hasEfficiencyEnchant = false;
+        for (Map.Entry<Enchantment, Integer> entry : itemEnchants.entrySet()) {
+            Enchantment enchant = entry.getKey();
+            String enchantName = EnchantmentHelper.getEnchantmentId(enchant).toString();
+            if (enchantName.equals("minecraft:efficiency")) {
+                hasEfficiencyEnchant = true;
+                break;
             }
         }
+
+        if (!hasEfficiencyEnchant) {
+            return;
+        }
+        /* Player has an item with efficiency enchantment */
+        CarboxyForgeMod.LOGGER.info("[PlayerMixin] Dropped item has Efficiency on it");
+        Player player = (Player) (Object) this;
+
+        player.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 20 * 5, 2));
+        
+    
     }
 
 
